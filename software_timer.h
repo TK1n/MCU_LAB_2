f6e7c39118dcb3600364daba0b0ebad47c453335
TKin <trungkienjoe58@gmail.com>
2023-09-28 16:39:13 +0700

    Ex1 with software_timer

---
+++ software_timer.h
@@ -0,0 +1,16 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: 28 thg 9, 2023
+ *      Author: PC
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
